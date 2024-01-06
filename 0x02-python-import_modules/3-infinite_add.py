8cdd6d07c11b2dee4c86913d8216553c11750923
samuel abiona <samuelo.abiona@gmail.com>
2024-01-06 03:07:59 -0800

    Second commit for the python import and module

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    '''Print the addition of all arguments.'''
+    """Print the addition of all arguments."""
     import sys
 
-    tot = 0
+    total = 0
     for i in range(len(sys.argv) - 1):
-        tot += int(sys.argv[i + 1])
-    print("{}".format(tot))
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
