a69524280295810a7ffb2c6e0228852bf1fd18b7
samuel abiona <samuelo.abiona@gmail.com>
2024-02-05 08:06:26 -0800

    First commit on python inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represents base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
+
