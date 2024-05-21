9ad73d273da70695df17d8f25af206b81491daa6
samuel abiona <samuelo.abiona@gmail.com>
2024-05-21 08:39:33 -0700

    Second correction for file 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,5 @@ const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
   if (err) {
     console.log(err);
-  }else {
-    console.log('Files written successfully');
-}
+  }
 });
